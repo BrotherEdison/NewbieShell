af6214642c3f33f7e7fcb747d0234ee31a80fc10
BrotherEdison <brotheredison@outlook.com>
2014-11-09 20:15:54 +0800

    Create net.newbieshell.bootstrap.Main

---
+++ src/net/newbieshell/bootstrap/Main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# net.newbieshell.bootstrap.Main
+# This source file is part of the NewbieShell project.
+
+# Version Identification
+
+MAJORVER=0
+MINORVER=0
+PATCHVER=1
+SUBLEVEL=-dev
+
+# Variables
+
+
+
+# Functions
+
+
+
+# Runtime
