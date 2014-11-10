39867ae7ee51be42a97d7e57344543a0b39909a4
BrotherEdison <brotheredison@outlook.com>
2014-11-10 15:41:55 +0800

    Create net.newbieshell.client.Main

---
+++ src/net/newbieshell/client/Main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# net.newbieshell.client.Main
+# This source file is part of the NewbieShell project.
+
+# Version Identification
+
+MAJORVER=0
+MINORVER=0
+PATCHVER=1
+SUBLEVEL=-dev
+
+# Variables
+
+
+
+# Functions
+
+
+
+# Runtime
