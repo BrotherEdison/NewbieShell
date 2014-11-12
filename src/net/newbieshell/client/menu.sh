84acbd0b714802e26fed9f7319c93746fa006a4e
BrotherEdison <brotheredison@outlook.com>
2014-11-12 19:24:39 +0800

    Update net.newbieshell.client.menu
    + Added better error handling for checkIfInitialLogin()
    + Added catchInitialLoginException()
    
    Signed-off-by: BrotherEdison <brotheredison@outlook.com>

--- src/net/newbieshell/client/menu.sh
+++ src/net/newbieshell/client/menu.sh
@@ -6,7 +6,7 @@
 
 MAJORVER=0
 MINORVER=0
-PATCHVER=1
+PATCHVER=2
 SUBLEVEL=-dev
 
 # Variables
@@ -22,8 +22,10 @@ HOUR=$(date +%H)
 checkIfInitialLogin() {
 	if [ $initialLogin = 0 ]; then
 		greeting='Welcome back, '
+	elif [ $initialLogin = 1 ]; then
+		greeting='Welcome to NewbieShell, '
 	else
-		greeting='Hello, '
+		return 2779096485
 	fi
 }
 checkGreetTime() {
@@ -41,8 +43,19 @@ catchGreetTimeException() {
 	if [ $? = 464367618 ]; then
 		cat <<- _EOF_
 [$TIME WARN] Caught exception: Could not initialize variable greetHour.
-[$TIME WARN] Caused by: function checkGreetTime() (at net.newbieshell.client.menu:29)
+[$TIME WARN] Caused by: function checkGreetTime() (at net.newbieshell.client.menu:31)
 [$TIME WARN] This can be safely ignored, but 24-hour time will not function.
+_EOF_
+	else
+		return 0
+	fi
+}
+catchInitialLoginException() {
+	if [ $? = 2779096485 ]; then
+		cat <<- _EOF_
+[$TIME WARN] Caught exception: Could not initialize variable greeting.
+[$TIME WARN] Caused by: function checkIfInitialLogin() (at net.newbieshell.client.menu:22)
+[$TIME WARN] This can be safely ignored, but I will use the default greeting.
 _EOF_
 	else
 		return 0
