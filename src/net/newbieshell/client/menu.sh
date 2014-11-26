1eb7edc89038a592031d8c3dfd438d9f740e7c44
BrotherEdison <brotheredison@outlook.com>
2014-11-26 13:58:42 +0800

    Update net.newbieshell.client.menu
    + Added very basic Multiuser support
    
    Signed-off-by: BrotherEdison <brotheredison@outlook.com>

--- src/net/newbieshell/client/menu.sh
+++ src/net/newbieshell/client/menu.sh
@@ -24,8 +24,28 @@ menu() {
 	echo $greeting", "$userName", and good "$greetHour"."
 	echo "It is currently "$DATETIME"."
 	echo
-	prompt
+	sumu
 }
+sumu() {
+	echo "Do you wish to use Singleuser for this session, or connect to a Multiuser host? [S/M]"
+	read -n 1
+	if [ $REPLY = S ]; then
+		prompt
+	elif [ $REPLY = M ]; then
+		connect
+	else
+		prompt
+	fi
+}
+connect() {
+	echo "NewbieShell Multiuser is currently very buggy. Report any known bugs to <https://github.com/BrotherEdison/NewbieShell/issues> so we can squash them."
+	echo "Enter the IP, hostname, or web address of the Multiuser host."
+	read mu_hostname
+	echo "Enter the username for connecting to the Multiuser host. (Leave blank to assume current username)"
+	read mu_username
+	if [ -z $mu_username ]; then
+		mu_username=$userName
+	fi
 prompt() {
 	read -p "$username is in $pwd>" cmd
 	if cat $programList | grep $cmd ]; then
