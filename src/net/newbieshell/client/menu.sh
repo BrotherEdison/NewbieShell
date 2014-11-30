5d8195d3cd5cef697f2b6eaca9cfa5ba6233885c
BrotherEdison <brotheredison@outlook.com>
2014-11-30 18:14:11 +0800

    Update net.newbieshell.client.menu
    * Major overhaul: There is now a real menu, as said on the tin. This has not been true before 0.1.6.
    * Major overhaul: Singleuser has been abolished in a technical sense; it is now really connecting to 127.0.0.1.
    
    Signed-off-by: BrotherEdison <brotheredison@outlook.com>

--- src/net/newbieshell/client/menu.sh
+++ src/net/newbieshell/client/menu.sh
@@ -6,7 +6,7 @@
 
 MAJORVER=0
 MINORVER=1
-PATCHVER=6
+PATCHVER=7
 SUBLEVEL=
 
 # Variables
@@ -16,29 +16,45 @@ DATE=$(date +%x)
 TIME=$(date +%X)
 HOUR=$(date +%H)
 PATH=$(pwd)
-programList='/etc/nsh/programs'
 
 # Functions
 
 menu() {
+	clear
+	unset REPLY
 	echo $greeting", "$userName", and good "$greetHour"."
 	echo "It is currently "$DATETIME"."
-	echo
-	sumu
-}
-sumu() {
-	echo "Do you wish to use Singleuser for this session, or connect to a Multiuser host? [S/M]"
-	read -n 1
-	if [ $REPLY = S ]; then
-		prompt
+	echo "Please select a choice:"
+	echo "[S]ingleuser"
+	echo "[M]ultiuser"
+	echo "S[e]ttings"
+	echo "[Q]uit"
+	read -N 1
+	if [ -z $REPLY ]; then
+		menu
+	elif [ $REPLY = S ]; then
+		su_menu
 	elif [ $REPLY = M ]; then
-		connect
+		mu_menu
+	elif [ $REPLY = E ]; then
+		settings
+	elif [ $REPLY = Q ]; then
+		exit 0
 	else
-		prompt
+		menu
 	fi
 }
-connect() {
-	echo "NewbieShell Multiuser is currently very buggy. Report any known bugs to <https://github.com/BrotherEdison/NewbieShell/issues> so we can squash them."
+
+su_menu() {
+	echo "Parent: Starting integrated host..."
+	echo "Parent: Integrated host started on 127.0.0.1:30676..."
+	echo "Parent: Spawning connection process..."
+	mu_hostname=127.0.0.1
+	source net/newbieshell/server/connect/ssh.sh
+	exit
+}
+mu_menu() {
+		echo "NewbieShell Multiuser is currently very buggy. Report any known bugs to <https://github.com/BrotherEdison/NewbieShell/issues> so we can squash them."
 	echo "Enter the IP, hostname, or web address of the Multiuser host."
 	read mu_hostname
 	echo "Enter the username for connecting to the Multiuser host. (Leave blank to assume current username)"
@@ -57,21 +73,14 @@ connect() {
 		echo "Failed to connect to the host"
 		echo "Unknown host"
 	else
-		echo "Parent: Starting connection process."
-		source net/newbieshell/server/connect/ssh.sh &
-		pid=$!
-		echo "Parent: Waiting for connection process to exit..."
-		wait $pid
-		echo "Parent: Connection process terminated."
+		echo "Parent: Spawning connection process..."
+		source net/newbieshell/server/connect/ssh.sh
 		exit
 	fi
-prompt() {
-	read -p "$username is in $pwd>" cmd
-	if cat $programList | grep $cmd ]; then
-		$cmd
-	else
-		echo $cmd": command not found..."
-	fi
+}
+settings() {
+	# Still a work in progress
+	menu
 }
 # Runtime
 
