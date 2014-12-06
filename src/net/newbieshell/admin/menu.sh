77763bc67f6073c0723ea07b9d78a6eb855d2935
BrotherEdison <brotheredison@outlook.com>
2014-12-06 14:08:26 +0800

    Update net.newbieshell.admin.menu
    * Fixed another bug induced by last commit

--- src/net/newbieshell/admin/menu.sh
+++ src/net/newbieshell/admin/menu.sh
@@ -6,7 +6,7 @@
 
 MAJORVER=0
 MINORVER=1
-PATCHVER=11
+PATCHVER=13
 SUBLEVEL=
 
 # No variables for this module
@@ -20,7 +20,7 @@ greetMenu() {
   if [ $REPLY = I ]; then
     source net/newbieshell/admin/interactive.sh
   elif [ $REPLY = S ]; then
-    source net/newbieshell/admin/shell/shell.sh
+    source net/newbieshell/admin/shell.sh
   else
     greetMenu
   fi
