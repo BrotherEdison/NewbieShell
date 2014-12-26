b74ae67ead057ccfd6705095ada463d42cd0fff6
BrotherEdison <brotheredison@outlook.com>
2014-12-26 21:27:24 +0800

    Update net.newbieshell.server.connect.ssh
    + Added support for SysV-like inits
    + Added cleaning up (stopping sshd) after disconnection if the hostname was localhost
    * Fixed a bug in which it would use the wrong variable for the hostname

--- src/net/newbieshell/server/connect/ssh.sh
+++ src/net/newbieshell/server/connect/ssh.sh
@@ -6,7 +6,7 @@
 
 MAJORVER=0
 MINORVER=2
-PATCHVER=1
+PATCHVER=2
 SUBLEVEL=
 
 # Variables
@@ -24,9 +24,20 @@ findConnectionPort() {
 }
 remote_connect() {
 	echo "Child: Opening remote connection for "$mu_username" to "$trunc_hostname":"$trunc_connectionPort"..."
-	ssh -p $trunc_connectionPort $mu_username@$trunc_username
-	echo "Child: Remote connection closed. Exiting."
-	exit
+	ssh -p $trunc_connectionPort $mu_username@$trunc_hostname:$trunc_connectionPort
+	echo "Child: Remote connection closed. Finishing up..."
+    if [ $mu_hostname = 127.0.0.1 ]; then
+        if [ $INITTYPE = systemd ]; then
+            systemctl stop sshd.service
+        elif [ $INITTYPE = sysv-deb ]; then
+            service stop ssh
+        elif [ $INITTYPE = sysv-rh ]; then
+            service start sshd
+        fi
+    else
+        exit
+    fi
+    exit
 }
 
 # Runtime
